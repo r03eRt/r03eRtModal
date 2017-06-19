22d0997fa6893e26058079a183aedb70e2b1cd44
rmorgado <!-- Top Banner  --	 aside id="top-banner" class="container"	 	div data-js-ad-manager-slot="div-hola-slot-megabanner"/div /aside>
2017-06-19 13:53:48 +0200

    added test chai and mocha to this project

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+var r03ertmodal = require('./index');
+
+describe('r03ertmodal', function()
+{
+	it('should work!', function()
+	{
+		expect(true).to.be.true;
+	});
+});
